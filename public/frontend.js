5ddc4e2b7630aa931bb5e3a2ef201274a607ea71
Todd Briley <todd.briley@outlook.com>
2017-04-19 21:56:52 -0400

    updated styling and functionality

--- public/frontend.js
+++ public/frontend.js
@@ -1,11 +1,16 @@
+var ctxcolor;
+
+
 $(function () {
-  console.log("loaded");
   var mousedown =false;
   var lastx = -1;
   var lasty = -1;
   var socket = io();
-  var ctxcolor = 'blue';
+  ctxcolor = '#000000';
   var penSize = 5;
+  var canvasTopOffset = $('canvas').offset().top;
+  var canvasLeftOffset = $('canvas').offset().left;
+
   socket.emit('login');
   $('.color').click(function(){
       ctxcolor = $(this).attr('id');
@@ -18,47 +23,45 @@ $(function () {
   });
 
   $('.eraser').click(function(){
-    ctxcolor= 'white';
+    ctxcolor = '#ffffff';
 
   });
 
   $('canvas').on('mousedown', function eventHandler(event) {
-    // console.log("mouse down in canvas");
     mousedown = true;
 
-    lastx = event.clientX;
-    lasty = event.clientY;
+    lastx = event.clientX - canvasLeftOffset;
+    lasty = event.clientY - canvasTopOffset;
 
     });
 
   $('canvas').on('mouseup', function eventHandler(event) {
-    // console.log("mouse up in canvas");
     mousedown =false;
     lastx = -1;
     lasty = -1;
     });
 
     $('canvas').on('mousemove', function eventHandler(event) {
-      // console.log("mouse move in canvas");
       if (mousedown) {
-        // console.log("mouse is down and moving");
         ctx.strokeStyle = ctxcolor;
 
         ctx.lineWidth = penSize;
         ctx.beginPath();
+        // ctx.moveTo(lastx + canvasLeftOffset, lasty + canvasTopOffset);
         ctx.moveTo(lastx, lasty);
 
-        var x = event.clientX;
-        var y = event.clientY;
+        var x = event.clientX - canvasLeftOffset;
+        var y = event.clientY - canvasTopOffset;
 
+        // ctx.lineTo(x + canvasLeftOffset, y + canvasTopOffset);
         ctx.lineTo(x, y);
         ctx.lineJoin = 'round';
         ctx.closePath();
         ctx.stroke();
         var tempx = x;
         var tempy = y;
-        // console.log(lastx,lasty, tempx, tempy);
-        socket.emit('drawing', {startx:lastx, starty:lasty, endx: tempx, endy:tempy, color: ctxcolor, pensize: penSize});
+        hexValueWithoutHashSymbol = ctxcolor.toString().substring(1,7);
+        socket.emit('drawing', {startx:lastx, starty:lasty, endx: tempx, endy:tempy, color: hexValueWithoutHashSymbol, pensize: penSize});
         lastx = x;
         lasty = y;
       }
@@ -66,14 +69,10 @@ $(function () {
 
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-  // var username = prompt("what is your name?");
 
 
     socket.on('drawing', function(data) {
-      // console.log('startx: ' + data.startx);
-      // console.log('color: ' + data.color);
-      // console.log('endx: ' + data.endx);
-      ctx.strokeStyle = data.color;
+      ctx.strokeStyle = '#' + data.color;
       ctx.lineWidth = data.pensize;
       ctx.beginPath();
       ctx.moveTo(data.startx,data.starty);
@@ -83,9 +82,11 @@ $(function () {
 
     });
 
-    socket.on('login', function(data) {
-      // console.log('color given: ' + data.color);
-      ctxcolor = data.color;
-    });
+    $(window).resize(getCanvasOffsets);
+    function getCanvasOffsets(){
+      canvasTopOffset = $('canvas').offset().top;
+      canvasLeftOffset = $('canvas').offset().left;
+
+    }
 
 });
